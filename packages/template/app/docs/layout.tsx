e40786ebc1e0df8ff70c103c9e9fb00925f4121e
Oxford Pierpont <admin@oxpi.co>
2026-05-16 19:38:31 -0400

    Use editable docs navigation content

--- packages/template/app/docs/layout.tsx
+++ packages/template/app/docs/layout.tsx
@@ -3,6 +3,7 @@ import { DocsSidebar } from '../components/docs/docs-sidebar'
 import { DocsHeader } from '../components/docs/docs-header'
 import { DocsNavTabs } from '../components/docs/docs-nav-tabs'
 import { siteConfig } from '@/lib/theme-config'
+import siteContent from '@/lib/site-content.json'
 
 export default function DocsLayout({
   children,
@@ -19,7 +20,7 @@ export default function DocsLayout({
       {/* Navigation tabs (Documentation / API Reference) */}
       <div className="sticky top-16 z-30 bg-background border-b border-border hidden lg:block">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <DocsNavTabs />
+          <DocsNavTabs tabs={siteContent.topNav} />
         </div>
       </div>
 
